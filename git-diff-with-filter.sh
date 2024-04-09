9513d19e87b5c4a84b6b32aa4f77977d5e82c2ca
c4arl0s <c.santiago.cruz@gmail.com>
2024-04-09 15:02:31 -0600

    Add first commit

---
+++ git-diff-with-filter.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PARAMETER=$1
+let counter=0
+git-status-with-filter ${PARAMETER} | tail -n +2 |  sed '$d' | while read modified_file; do
+  echo "================="
+  let "COUNTER+=1"
+  echo "${counter} - ${modified_file} 🟢"
+  echo "================="
+  
+ file=$(echo "${modified_file}")
+ git --no-pager diff --color=always "${file}" | less -R
+done
